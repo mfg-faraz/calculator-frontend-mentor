239f32bb94c12bdda16956fc7f3dcde709cdb01b
mfg-faraz <mfgfaraz@gmail.com>
2023-06-03 16:16:11 +0500

    update README

--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useEffect } from "react";
 
 const Main = ({ children }) => {
+  useEffect(()=>{
+document.body.className="bg-background-main"
+  },[])
   return (
-    <main className="flex justify-center items-center flex-col min-h-screen bg-background-main px-3 sm:p-0 transition-all duration-300">
+    <main className="flex justify-center items-center flex-col min-h-screen  px-3 sm:p-0 transition-all duration-300">
       {children}
     </main>
   );
